fa23a7ffd1e7f0fcf4665595965c38bd16f0fc80
segawa74 <55907380+segawa74@users.noreply.github.com>
2024-10-28 21:39:26 +0900

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/SegawaSite/",
+  base: "/segawa-site/",
   plugins: [react()],
   resolve: {
     alias: {
